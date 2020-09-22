0e9dda124812d9a52d2c178941b3dfc2695a2270
ednadev <ednadev@naver.com>
2020-09-22 14:53:22 +0900

    OAuth2 로그인, 회원가입, 로그아웃 기능 구현

--- src/main/java/com/project/spring/web/IndexController.java
+++ src/main/java/com/project/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.project.spring.web;
 
+import com.project.spring.config.auth.dto.SessionUser;
 import com.project.spring.service.gallery.GalleryService;
 import com.project.spring.service.posts.PostsService;
 import com.project.spring.web.dto.GalleryResponseDto;
@@ -10,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.swing.*;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,11 +19,17 @@ public class IndexController {
 
     private final PostsService postsService;
     private final GalleryService galleryService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         model.addAttribute("gallery", galleryService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user!=null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
